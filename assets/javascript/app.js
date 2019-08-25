4db3ee224a0cae2fa293bfc1afcb1434360e832d
Terrance Bryant <tj904615@gmail.com>
2019-08-24 21:41:54 -0500

    Final product

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 //Create var for timer
 var betweenTime;
 function startClock(duration, display) {
-    const timer = duration;
-    const seconds;
-    const minutes;
+    var timer = duration;
+    var seconds;
+    var minutes;
 
     betweenTime=setInterval(function() {
         console.log(betweenTime);
@@ -46,3 +46,45 @@ var questions = [
     [ "How many points did I score in my final game?", "19", "60", "31", "0", "B" ],
 ];
 
+function _(x){
+    return document.getElementById(x);
+}
+
+function renderQuestion(){
+    test = _("test");
+    if(pos >= questions.length){
+        test.innerHTML = "<h2>You have "+correct+" out of "+questions.length+" questions correct</h2>";
+        _("test_status").innerHTML = "Test Completed";
+        pos = 0;
+        correct = 0;
+        return false;
+    }
+    _("test_status").innerHTML = "Question "+(pos+1)+" of "+questions.length;
+    question = questions[pos][0];
+    chA = questions[pos][1];
+    chB = questions[pos][2];
+    chC = questions[pos][3];
+    chD = questions[pos][4];
+    test.innerHTML = "<h3>"+question+"</h3>";
+    test.innerHTML += "<input type='radio' name='choices' value='A'> "+chA+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='B'> "+chB+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='C'> "+chC+"<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='D'> "+chD+"<br><br>";
+    test.innerHTML += "<button onclick='checkAnswer() '>Submit Answer</button>";
+}
+
+function checkAnswer(){
+    choices = document.getElementsByName("choices");
+    for(var i=0; i<choices.length; i++){
+        if(choices[i].checked){
+            choice = choices[i].nodeValue;
+        }
+    }
+    if(choice == questions[pos][5]){
+        correct++;
+    }
+    pos++;
+    renderQuestion();
+}
+
+window.addEventListener("load", renderQuestion, false);
