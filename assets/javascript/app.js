16e91058a88a7fc66f6f4be9b5ceee595b14ebe7
Terrance Bryant <tj904615@gmail.com>
2019-08-24 19:52:50 -0500

    created questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,9 +23,26 @@ function startClock(duration, display) {
             seconds = seconds < 10 ? "0" + ":" + seconds : seconds;
             minutes = minutes < 10 ? "0" + ":" + minutes : minutes;
 
-            
-
+            display.textContent = seconds + ":" + minutes;
+            console.log(timer);
 
         }
-    })
+        timer--;
+    }, 1000);
 }
+window.onload = function () {
+    var sevenMin = 7 * 60,
+        display = document.querySelector("#time");
+        startClock(sevenMin, display);
+};
+
+var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
+var questions = [
+    [ "What year was I drafted?", "1995", "2001", "1996", "1999", "C" ],
+    [ "What number draft pick was I?", "1st", "8th", "Undrafted", "13th", "D" ],
+    [ "What team drafted me?", "Lakers", "76ers", "Hornets", "Bucks", "C" ],
+    [ "In what city was I born?", "Jacksonville", "Paris", "Oakland", "Philly", "D" ],
+    [ "What is my career high points in a game", "81", "60", "71", "100", "A" ],
+    [ "How many points did I score in my final game?", "19", "60", "31", "0", "B" ],
+];
+
